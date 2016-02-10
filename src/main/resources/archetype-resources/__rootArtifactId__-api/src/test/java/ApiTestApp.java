dee570c41b87e1b895cb61091b87c866f0c7687f
wengyingjian <q291611265@gmail.com>
2016-02-11 01:33:53 +0800

    enable test app runner

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-api/src/test/java/ApiTestApp.java
@@ -0,0 +1,11 @@
+package ${package};
+
+/**
+ * Created by wengyingjian on 16/2/11.
+ */
+public class ApiTestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApp.class, args);
+    }
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/test/java/ServiceTestApp.java
@@ -0,0 +1,12 @@
+package ${package};
+
+
+/**
+ * Created by wengyingjian on 16/2/11.
+ */
+public class ServiceTestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApp.class, args);
+    }
+}
