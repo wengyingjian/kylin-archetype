cae698444ac078fbf97af67c2990322ad44049fc
wengyingjian <q291611265@gmail.com>
2016-02-13 16:37:42 +0800

    enable tomcat deploy

--- src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/ApiApp.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/ApiApp.java
@@ -7,12 +7,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.wengyingjian.kylin", "${package}"})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class ApiApp {
+public class ApiApp extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(ApiApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApp.class);
     }
 }
--- src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/ServiceApp.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/ServiceApp.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.wengyingjian.kylin", "${package}"})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class ServiceApp {
+public class ServiceApp extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(ServiceApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServiceApp.class);
     }
 }
